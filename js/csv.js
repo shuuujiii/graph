eafd2513ece5d61248c7b96e9f9a81956084efc0
shuuujiii <rinkioutouhenmonzizai@yahoo.co.jp>
2019-09-03 16:00:36 +0900

    add csv upload button

--- js/csv.js
+++ js/csv.js
@@ -15,17 +15,72 @@ function drawBarChart(data) {
   for (var row in data) {
     tmpLabels.push(data[row][0])
     tmpData1.push(data[row][1])
-    tmpData2.push(data[row][2])
-  };  // 4)chart.jsで描画
-  var ctx = document.getElementById("myChart").getContext("2d");
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: tmpLabels,
-      datasets: [
-        { label: "Tokyo", data: tmpData1, backgroundColor: "red" },
-        { label: "Osaka", data: tmpData2, backgroundColor: "blue" }
-      ]
-    }
-  });
+};  // 4)chart.jsで描画
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+	   type: 'line',
+	   data: {
+	      labels: tmpLabels, 
+	      datasets: [
+	      {
+		 label: "Chart-1",
+		 borderColor: 'rgb(255, 0, 0)',
+		 data: tmpData1, 
+		 pointHoverRadius: 10,
+		 pointRadius: 10,
+	      },
+	      ]
+	   },
+	   options: {
+	     hover: {
+	       mode: 'point'
+	     },
+	     onClick: function(event, elements) {
+	       console.log(`Found ${elements.length} elements`);
+	       elements.forEach(function(element) {
+		 let point = myChart.data.datasets[element._datasetIndex].data[element._index];
+		 console.log(point);
+	       })
+	     },
+	     scales: {
+	       xAxes: [{
+		 ticks: {
+		   beginAtZero: true
+		 }
+	       }],
+	       yAxes: [{
+		 ticks: {
+		   beginAtZero: true
+		 }
+	       }]
+	     },
+	     tooltips: {
+	       callbacks: {
+		 title: function(tooltipItem, data) {
+		   return "aaa"; 
+		 },
+		 afterTitle: function(tooltipItem, data) {
+		   return "afterTitle";
+		 },
+		 label: function(tooltipItem, data) {
+		   return "labeldesu";
+		 }
+	       },
+	       titleFontSize: 100,
+	       bodyFontSize: 100
+	     }
+	  }
+	});
+	canvas.addEventListener('click', function(event) {
+	let item = myChart.getElementAtEvent(event);
+	if (item.length == 0) {
+		console.log('no element found.')
+		    return;
+	}
+	item = item[0];
+	let data = item._chart.config.data.datasets[item._datasetIndex].data[item._index];
+	alert(`Clicked at `);
+	window.location.href = './index.html'; // 通常の遷移
+    });
+
 }
