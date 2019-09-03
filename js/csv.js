7786557f571c34584df0d16aa1b0732c2a69a857
shuuujiii <rinkioutouhenmonzizai@yahoo.co.jp>
2019-09-03 11:22:37 +0900

    first commit

---
+++ js/csv.js
@@ -0,0 +1,31 @@
+// 2) CSVから２次元配列に変換
+function csv2Array(str) {
+  var csvData = [];
+  var lines = str.split("\n");
+  for (var i = 0; i < lines.length; ++i) {
+    var cells = lines[i].split(",");
+    csvData.push(cells);
+  }
+  return csvData;
+}
+
+function drawBarChart(data) {
+  // 3)chart.jsのdataset用の配列を用意
+  var tmpLabels = [], tmpData1 = [], tmpData2 = [];
+  for (var row in data) {
+    tmpLabels.push(data[row][0])
+    tmpData1.push(data[row][1])
+    tmpData2.push(data[row][2])
+  };  // 4)chart.jsで描画
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: tmpLabels,
+      datasets: [
+        { label: "Tokyo", data: tmpData1, backgroundColor: "red" },
+        { label: "Osaka", data: tmpData2, backgroundColor: "blue" }
+      ]
+    }
+  });
+}
