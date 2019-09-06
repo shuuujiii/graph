e742e115839f792a2814a8db312975b74982a514
shuuujiii <rinkioutouhenmonzizai@yahoo.co.jp>
2019-09-06 16:55:17 +0900

    version0.1

--- js/csv.js
+++ js/csv.js
@@ -1,97 +1,107 @@
 // 2) CSVから２次元配列に変換
+var labels = [];
+var values = [];
+var titles = [];
+var urls = [];
+var csvColIndex = {label : 0, value : 1, title : 2, url : 3};
 function csv2Array(str) {
-	var csvData = [];
-  var lines = str.split("\n");
-  for (var i = 0; i < lines.length; ++i) {
-    var cells = lines[i].split(",");
-    csvData.push(cells);
-  }
-  return csvData;
+    var csvData = [];
+    var lines = str.split("\n");
+    for (var i = 0; i < lines.length; ++i) {
+        var cells = lines[i].split(",");
+        csvData.push(cells);
+    }
+    return csvData;
 }
 
-function chartData(data){
-   var tmpLabels = [], tmpData1 = [], tmpData2 = [];
-   for (var row in data) {
-     tmpLabels.push(data[row][0]);
-     tmpData1.push(data[row][1]);
-   };
-   return  {
-	      labels: tmpLabels, 
-	      datasets: [
-	      {
-		 label: "Chart-1",
-		 borderColor: 'rgb(255, 0, 0)',
-		 data: tmpData1, 
-		 pointHoverRadius: 10,
-		 pointRadius: 10,
-	      },
-	      ]
-	   };
+function setData(csvData){
+    clearData();
+    for (var row in csvData) {
+        labels.push(csvData[row][csvColIndex.label]);
+        values.push(csvData[row][csvColIndex.value]);
+        titles.push(csvData[row][csvColIndex.title]);
+        urls.push(csvData[row][csvColIndex.url]);
+    };
+}
+
+function clearData(){
+    labels = [];
+    values = [];
+    titles = [];
+    urls = [];
+}
+function chartData(){
+    return  {
+        labels: labels,
+        datasets: [
+            {
+                label: "Chart-1",
+                borderColor: 'rgb(255, 0, 0)',
+                data: values, 
+                pointHoverRadius: 10,
+                pointRadius: 10,
+            },
+        ]
+    };
+}
+
+function chartOptionTooltip(){
+    return {
+        callbacks: {
+            title: function(tooltipItem, data) {
+                return titles[tooltipItem[0].index];
+                //return data[tooltipItem[0].index]; 
+                //return getTooltipTitle(1);
+            },
+            label: function(tooltipItem, data) {
+                return "";
+            }
+        },
+        titleFontSize: 30,
+        bodyFontSize: 30
+    }
 }
 
 function createChart(data, canvas, ctx)
 {
-	return new Chart(ctx, {
-	   type: 'line',
-	   data: data,
-	   options: {
-	     hover: {
-	       mode: 'point'
-	     },
-	     onClick: function(event, elements) {
-	       console.log(`Found ${elements.length} elements`);
-	       elements.forEach(function(element) {
-		 let point = myChart.data.datasets[element._datasetIndex].data[element._index];
-		 console.log(point);
-	       })
-	     },
-	     scales: {
-	       xAxes: [{
-		 ticks: {
-		   beginAtZero: true
-		 }
-	       }],
-	       yAxes: [{
-		 ticks: {
-		   beginAtZero: true
-		 }
-	       }]
-	     },
-	     tooltips: {
-	       callbacks: {
-		 title: function(tooltipItem, data) {
-		   return "aaa"; 
-		 },
-		 afterTitle: function(tooltipItem, data) {
-		   return "afterTitle";
-		 },
-		 label: function(tooltipItem, data) {
-		   return "labeldesu";
-		 }
-	       },
-	       titleFontSize: 100,
-	       bodyFontSize: 100
-	     }
-	  }
-	});
-}
-function drawBarChart(data, canvas, ctx) {
-  // 3)chart.jsのdataset用の配列を用意
-   // 4)chart.jsで描画
-	myChart = createChart(data, canvas, ctx);
-	myChart.update();
-        setCanvasClick(myChart, canvas);
+    return new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            hover: {
+                mode: 'point'
+            },
+            onClick: function(event, elements) {
+                console.log(`Found ${elements.length} elements`);
+                elements.forEach(function(element) {
+                    let point = myChart.data.datasets[element._datasetIndex].data[element._index];
+                    console.log(point);
+                })
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+        }
+    });
 }
 function setCanvasClick(myChart, canvas){
-	canvas.addEventListener('click', function(event) {
-	let item = myChart.getElementAtEvent(event);
-	if (item.length == 0) {
-		console.log('no element found.')
-		    return;
-	}
-	item = item[0];
-	let data = item._chart.config.data.datasets[item._datasetIndex].data[item._index];
-	alert(`Clicked at `);
-	window.location.href = './index.html'; // 通常の遷移
+    canvas.addEventListener('click', function(event) {
+        let item = myChart.getElementAtEvent(event);
+        if (item.length == 0) {
+            console.log('no element found.')
+            return;
+        }
+        item = item[0];
+        let data = item._chart.config.data.datasets[item._datasetIndex].data[item._index];
+        window.location.href = urls[item._index]; // 通常の遷移
     });
 }
